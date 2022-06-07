891e4920b7d32c583310e5aaaff5adcff5f063e9
Samuel Viana <samuelviana.work@gmail.com>
2022-06-07 19:56:18 -0300

    commit inicial

---
+++ trab.cpp
@@ -0,0 +1,2 @@
+D:\SAMUEL\ProgramasFaculdade\Dev-Cpp\MinGW64\x86_64-w64-mingw32\bin\ld.exe	cannot open output file D:\SAMUEL\ProgEstr\trabA2\trab.exe: Permission denied
+D:\SAMUEL\ProgEstr\trabA2\collect2.exe	[Error] ld returned 1 exit status
