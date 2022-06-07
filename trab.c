366489da0eb7f7d1028359db228bfb5c0a9252d6
Samuel Viana <samuelviana.work@gmail.com>
2022-06-07 20:45:12 -0300

    agora vai

--- trab.c
+++ trab.c
@@ -1,5 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void main(){
-    printf("PQP EU SOH FACO MERDA!!!");
+void comandoInvalido;
+void mostraMenuPrincipal;
+void mostraMenuPruduto;
+void mostraMenuPedido;
+void incluiProduto;
+void alteraProduto;
+void excluiProduto;
+void consultaProduto;
+void listaProduto;
+
+
+
+
+
+
+ typedef struct Produto {
+ 	int codigo();
+ 	char descricao[51]();
+ 	int qtdEstoque();
+ 	double preco();
+ 
+ }produto;
+void comandoInvalido(){
+	printf("\nCOMANDO INVALIDO! TENTE NOVAMENTE!\n"); mostraMenuPrincipal;
 }
+
+void mostraMenuPrincipal(){
+	int(acao);
+	printf ("\nSeja bem vindo ao APP\nDigite 1 seguido de ENTER para GERENCIAR PRODUTO.\nDigite 2 seguido de ENTER para REGISTRAR PEDIDO DE COMPRA\nDigite seguido de ENTER para DESTRUIR A LOJA\n Digite seguido de ENTER 4 para encerrar a aplicacao.");
+	scanf("%d", &acao);
+	switch(acao)
+	case(1): mostraMenuProduto(); break;
+	case(2): mostraMenuPedido(); break;
+	
+	
+	default: comandoInvalido(); return;
+}
+
+void mostraMenuProduto(){
+	int(acao)
+	printf ("\nDigite 1 seguido de ENTER para INCLUIR PRODUTO\nDigite 2 seguido de ENTER para ALTERAR PRODUTO\n Digite 3 seguido de ENTER para EXCLUIR PRODUTO\n Digite 4 seguido de ENTER para CONSULTAR PRODUTO\n Digite 5 Seguido de ENTER para LISTAR PRODUTOS\n ");
+	
+}
+
+
+  };
+  
+ /* 
+  printf("\n---------- CADASTRO DE PRODUTOS -----------\n\n\n");
+  
+  printf("Nome do produto ......: ");
+  fflush(stdin);
+  fgets(produto.nomeProduto, 50, stdin);
+  
+  printf("Descricao ......: ");
+  fflush(stdin);
+  fgets(produto.descricaoProduto, 50, stdin);
+  
+  printf("Informe o codigo do produto ..: ");
+  scanf("%i", &produto.codigoProduto);
+  
+  printf("Informe o preco do produto ..: ");
+  scanf("%f", &produto.precoProduto);
+  
+  printf("Informe a quantidade no estoque ..: ");
+  scanf("%i", &produto.qtd_estoque);
+  
+  printf("\n\n --------- LENDO OS DADOS DO PRODUTO ---------\n\n");
+  printf("\n Nome ...........: %s\n", produto.nomeProduto);
+  printf("\n Descricao .....: %s\n", produto.descricaoProduto);
+  printf("\n Codigo ...: %i\n" , produto.codigoProduto);
+  printf("\n Preco ...: %f\n" , produto.precoProduto);
+  printf("\n Quantidade no estoque ...: %i\n" , produto.qtd_estoque);
+//CORREÇOES A SEREM APLICADAS:
+//N estou conseguindo colocar numero decimal quando preencho o preço do produto.
+//Colocar regras de preenchimento ao decorrer da aplicação.
+//OBS: O grupo deverá propor uma quantidade inicial de alocação. Quando o estiver cheio, deverá ser proposta uma estratégia para aumentar o tamanho do vetor
+  
+getch();
+return 0;  
+*/
+};
+};
