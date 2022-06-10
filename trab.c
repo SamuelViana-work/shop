689a41d6c6783fb6e8186fcf0458c37801d36bf8
Samuel Viana <samuelviana.work@gmail.com>
2022-06-10 17:38:28 -0300

    att

--- trab.c
+++ trab.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PRINCIPAL 1;
+#define PRDUTO 2;
+#define PEDIDO 3
+
 void comandoInvalido;
 void mostraMenuPrincipal;
 void mostraMenuPruduto;
@@ -17,36 +21,46 @@ void listaProduto;
 
 
  typedef struct Produto {
- 	int codigo();
- 	char descricao[51]();
- 	int qtdEstoque();
- 	double preco();
+ 	int codigo;
+ 	char descricao[51];
+ 	int qtdEstoque;
+ 	double preco;
  
  }produto;
+
 void comandoInvalido(){
 	printf("\nCOMANDO INVALIDO! TENTE NOVAMENTE!\n"); mostraMenuPrincipal;
 }
 
 void mostraMenuPrincipal(){
 	int(acao);
-	printf ("\nSeja bem vindo ao APP\nDigite 1 seguido de ENTER para GERENCIAR PRODUTO.\nDigite 2 seguido de ENTER para REGISTRAR PEDIDO DE COMPRA\nDigite seguido de ENTER para DESTRUIR A LOJA\n Digite seguido de ENTER 4 para encerrar a aplicacao.");
+	printf ("\nSeja bem vindo ao APP\nDigite 1 seguido de ENTER para GERENCIAR PRODUTO.\nDigite 2 seguido de ENTER para REGISTRAR PEDIDO DE COMPRA\nDigite 3 seguido de ENTER para DESTRUIR A LOJA\n Digite seguido de ENTER 4 para encerrar a aplicacao.");
 	scanf("%d", &acao);
-	switch(acao)
+	switch(acao){
 	case(1): mostraMenuProduto(); break;
 	case(2): mostraMenuPedido(); break;
 	
 	
-	default: comandoInvalido(); return;
+	default: comandoInvalido; return;
+}
 }
 
 void mostraMenuProduto(){
 	int(acao)
-	printf ("\nDigite 1 seguido de ENTER para INCLUIR PRODUTO\nDigite 2 seguido de ENTER para ALTERAR PRODUTO\n Digite 3 seguido de ENTER para EXCLUIR PRODUTO\n Digite 4 seguido de ENTER para CONSULTAR PRODUTO\n Digite 5 Seguido de ENTER para LISTAR PRODUTOS\n ");
-	
+	printf ("\nDigite 1 seguido de ENTER para INCLUIR PRODUTO\nDigite 2 seguido de ENTER para ALTERAR PRODUTO\n Digite 3 seguido de ENTER para EXCLUIR PRODUTO\n Digite 4 seguido de ENTER para CONSULTAR PRODUTO\n Digite 5 Seguido de ENTER para LISTAR PRODUTOS\nDigite 6 para RETORNAR AO MENU ANTERIOR ");
+	scanf("%d", &acao);
+	switch(acao){
+	case(1):incluiProduto();
+	case(2):alteraProduto();
+	case(3):excluiProduto();
+	case(4):consultaProduto();
+	case(5):listaProduto();
+	case(6):mostraMenuPrincipal();
+	default: comandoInvalido; return;
+}
 }
 
 
-  };
   
  /* 
   printf("\n---------- CADASTRO DE PRODUTOS -----------\n\n\n");
@@ -82,5 +96,4 @@ void mostraMenuProduto(){
 getch();
 return 0;  
 */
-};
-};
+
